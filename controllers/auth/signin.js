73a1e863e218c5427041eb6b5320effb7bf9ec89
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-10-19 19:51:45 +0300

    sum

--- controllers/auth/signin.js
+++ controllers/auth/signin.js
@@ -4,27 +4,34 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../../models/user");
 const { createReqError } = require("../../helpers");
 
-const { SECRET_KEY } = process.env;
+// const { SECRET_KEY } = process.env;
 
 async function signin(req, res) {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (!user) {
-    throw createReqError(401, "Email not found");
+    throw requestError(401, "Email is wrong");
   }
 
   const passwordCompare = await bcrypt.compare(password, user.password);
-
+  // if (!user.verify) {
+  //   throw createReqError(401, "Email not verify");
+  // }
   if (!passwordCompare) {
     throw createReqError(401, "Password is wrong");
   }
-  const userPayload = {
-    id: user._id,
-  };
-  const token = jwt.sign(userPayload, SECRET_KEY, { expiresIn: "6h" });
 
-  res.status(201).json({ email, token });
+  // create token //
+  const payload = { id: user._id };
+
+  const token = jwt.sign(payload, `${process.env.SECRET_KEY}`, {
+    expiresIn: "6h",
+  });
+  res.status(201).json({
+    token,
+    email: user.email,
+  });
 }
 
 module.exports = signin;
