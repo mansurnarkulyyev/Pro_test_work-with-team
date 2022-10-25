b4a152c56368990306c27b79436c131f790b4f8d
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-10-25 12:07:23 +0300

    api for materials

---
+++ routes/api/materials.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const { createTryCatchWrapper } = require("../../helpers");
+const { materialsTemplate } = require("../../models/materials");
+const { validateBody } = require("../../middleware");
+
+const { add, remove, getList } = require("../../controllers/materials");
+
+router.post(
+  "/add",
+  validateBody(materialsTemplate),
+  createTryCatchWrapper(add)
+);
+router.delete("/:id", createTryCatchWrapper(remove));
+router.get("/", createTryCatchWrapper(getList));
+
+module.exports = router;
