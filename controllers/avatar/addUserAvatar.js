edbf1b36a45d3e6ba0ad6c8ba8e1dd99e36dd319
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-10-29 21:12:46 +0300

    upd contacts add

--- controllers/avatar/addUserAvatar.js
+++ controllers/avatar/addUserAvatar.js
@@ -1,8 +1,7 @@
-
 const path = require("path");
 
 const { Contact } = require("../../models/contact");
-const fs = require("fs/promises"); 
+const fs = require("fs/promises");
 const { createReqError } = require("../../helpers");
 
 const teams = [];
@@ -10,46 +9,31 @@ const teams = [];
 const avatarDir = path.join(__dirname, "../../", "public", "contacts");
 
 const addUserAvatar = async (req, res) => {
+  try {
+    if (!req.user.admin) {
+      throw createReqError(403, "Not admin");
+    }
 
-    try {
-
-         if (!req.user.admin) {
-            throw createReqError(403, "Not admin")
-        }
-
-        const { path: tempUpload, originalname } = req.file;
-        const { _id } = req.user;
-        const extension = originalname.split(".").pop();
-        const filename = `${_id}.${extension}`;
-        const resultUpload = path.join(avatarDir, filename);
-        await fs.rename(tempUpload, resultUpload);
-        const avatarURL = path.join("contacts", filename);
-        const newAvatar = {
-        name: req.body.name,
-        position: req.body.position,
-        about: req.body.about,
-        cover:avatarURL,
+    const { path: tempUpload, originalname } = req.file;
+    const extension = originalname.split(".").pop();
+    const filename = `${originalname}.${extension}`;
+    const resultUpload = path.join(avatarDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("contacts", filename);
+    const newAvatar = {
+      name: req.body.name,
+      position: req.body.position,
+      about: req.body.about,
+      cover: avatarURL,
     };
-        
+
     const finalData = await Contact.create(newAvatar);
 
     res.status(201).json(finalData);
-    } catch (error) {
-        await fs.unlink(req.file.path);
-        res.status(401).json({status:401, error})
-    };
-
- };
+  } catch (error) {
+    await fs.unlink(req.file.path);
+    res.status(401).json({ status: 401, error });
+  }
+};
 
 module.exports = addUserAvatar;
-
-
-
-
-
-
-
-
-
-
-
