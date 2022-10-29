d77bf2bc0fc4997415dcc015079c46db0508eb57
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-10-29 19:43:32 +0300

    new names

---
+++ controllers/contacts/index.js
@@ -0,0 +1,9 @@
+const addContactAvatar = require("./addContactAvatar");
+const getAll = require("./getAll");
+const removeById = require("./removeById");
+
+module.exports = {
+  addContactAvatar,
+  getAll,
+  removeById,
+};
