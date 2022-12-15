8f2707a5d13c1bf6ef65af26ef4e74c2ffb3a8f6
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-12-15 02:53:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-require("dotenv").config();
-mongoose.set('strictQuery', true);
 const { DB_HOST, PORT=3004 } = process.env;
 
 mongoose
-  .createConnection(DB_HOST,{useNewUrlParser: true, useUnifiedTopology: true})
+  .createConnection(DB_HOST)
   .then(() => app.listen(PORT, () => console.log("Started ok")))
   .catch((err) => {
     console.log(err.message);
